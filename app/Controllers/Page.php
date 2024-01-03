88b04d9b59a4512f1db581c114a06e79036b6bbf
lairdtw <lairdcashflow@gmail.com>
2024-01-03 09:45:58 +0800

    initial

---
+++ app/Controllers/Page.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace App\Controllers;
+
+class Page extends BaseController
+{
+    public function index()
+    {
+        echo "2";
+    }
+
+    public function index2()
+    {
+        echo "3";
+    }
+}
